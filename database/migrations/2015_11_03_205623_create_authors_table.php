508c72de3dbfe40b3266a639829204629a8d412a
Antonio <antonio.lanzolla@gmail.com>
2015-11-03 21:58:30 +0100

    Aggiunta migration tabella Authors

---
+++ database/migrations/2015_11_03_205623_create_authors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->string('surname', 255);
+            $table->integer('book_id');
+            $table->foreign('book_id')->references('id')->to('books');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('authors');
+    }
+}
