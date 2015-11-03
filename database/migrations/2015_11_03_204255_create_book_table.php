391e30b65596947ae39b83705e24c719a437488d
Antonio <antonio.lanzolla@gmail.com>
2015-11-03 21:55:08 +0100

    Aggiunta migration Creazione tabella libri

---
+++ database/migrations/2015_11_03_204255_create_book_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 256);
+            $table->string('isbn', 18)->unique();
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('books');
+    }
+}
